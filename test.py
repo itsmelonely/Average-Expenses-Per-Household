8cd23978b93d294c46288140c1ce2158e98b61a3
Siwakorn Lertamnuaylap <60070095@kmitl.ac.th>
2017-11-29 22:06:52 +0700

    add test files I hope they have no bug

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 """test module"""
 
 import pandas as pd
+import numpy
+import pygal
 
 def read():
     """read csv"""
     data_frame = pd.read_csv('expenditure.csv')
-    print(data_frame)
+    data = (numpy.array(data_frame)).tolist()
+
+    #take the ","s out
+    data_ready = [[i.replace(',', '') if i.count(',') >= 1 else i for i in lst] for lst in data]
+
+
+    #create charts
+    line_chart = pygal.Line()
+    line_chart.title = 'Bungkan'
+    line_chart.x_title = 'Years'
+    line_chart.y_title = 'Baht'
+    line_chart.x_labels = map(str, range(2006, 2016))
+    line_chart.add(data_ready[56][0], [int(i) for i in data_ready[56][1:]])
+    line_chart.render_to_file('test.svg')
+
 
 read()
