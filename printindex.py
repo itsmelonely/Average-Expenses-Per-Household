ea9e6c012805ab7eb593fdc9862e734edab8a123
Siwakorn Lertamnuaylap <60070095@kmitl.ac.th>
2017-12-10 00:59:57 +0700

    add core graph

---
+++ printindex.py
@@ -0,0 +1,17 @@
+"""test"""
+
+import pandas as pd
+import numpy
+
+def test():
+    """read csv"""
+    data_frame = pd.read_csv('expenditure.csv')
+    data = (numpy.array(data_frame)).tolist()
+
+    #take the ","s out
+    data_ready = [[i.replace(',', '') if i.count(',') >= 1 else i for i in lst] for lst in data]
+
+    for i in range(len(data_ready)):
+        print(i, data[i][0])
+
+test()
